57db19e546e2ced4e1c449308c1fff7a12ec1478
André Schließer <a.schliesser@zeroseven.de>
2019-07-29 11:02:47 +0200

    Add image for php 7.3

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Alpine with composer, npm, yarn, gulp, typo3/surf etc.
 
 Choose your PHP version by using the matching tag:
 
-* `akoehnlein/surf:php56` PHP 5.6
-* `akoehnlein/surf:php70` PHP 7.0
-* `akoehnlein/surf:php71` PHP 7.1
-* `akoehnlein/surf:php72` PHP 7.2
+* `zeroseven07/surf:php56` PHP 5.6
+* `zeroseven07/surf:php70` PHP 7.0
+* `zeroseven07/surf:php71` PHP 7.1
+* `zeroseven07/surf:php72` PHP 7.2
+* `zeroseven07/surf:php73` PHP 7.3
 
-All available tags are listed on <https://hub.docker.com/r/akoehnlein/surf/tags/>
+All available tags are listed on <https://hub.docker.com/r/zeroseven07/docker-surf/tags>
--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 docker build --pull . -f php56/Dockerfile -t zeroseven07/surf:php56 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php56 /tmp/test.sh && \
 docker build --pull . -f php70/Dockerfile -t zeroseven07/surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php70 /tmp/test.sh && \
 docker build --pull . -f php71/Dockerfile -t zeroseven07/surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php71 /tmp/test.sh && \
-docker build --pull . -f php72/Dockerfile -t zeroseven07/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php72 /tmp/test.sh
+docker build --pull . -f php72/Dockerfile -t zeroseven07/surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php72 /tmp/test.sh && \
+docker build --pull . -f php73/Dockerfile -t zeroseven07/surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/surf:php73 /tmp/test.sh
