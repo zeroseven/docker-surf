3108be3fa4a079b6ac13bc055e0423fe864d4a40
André Schließer <a.schliesser@zeroseven.de>
2020-03-19 11:40:01 +0100

    Add php 7.4 image

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ docker build --pull . -f php56/Dockerfile -t zeroseven07/docker-surf:php56 && do
 docker build --pull . -f php70/Dockerfile -t zeroseven07/docker-surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php70 /tmp/test.sh && \
 docker build --pull . -f php71/Dockerfile -t zeroseven07/docker-surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php71 /tmp/test.sh && \
 docker build --pull . -f php72/Dockerfile -t zeroseven07/docker-surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php72 /tmp/test.sh && \
-docker build --pull . -f php73/Dockerfile -t zeroseven07/docker-surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php73 /tmp/test.sh
+docker build --pull . -f php73/Dockerfile -t zeroseven07/docker-surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php73 /tmp/test.sh && \
+docker build --pull . -f php74/Dockerfile -t zeroseven07/docker-surf:php74 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php74 /tmp/test.sh
