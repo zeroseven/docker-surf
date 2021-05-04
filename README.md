014649050cbb14ebee9e2a3ec52703c1828229ef
André Schließer <a.schliesser@zeroseven.de>
2021-05-04 09:13:59 +0200

    Use composer2 as default and provide new composer1 images

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Alpine with composer, npm, yarn, gulp, typo3/surf etc.
 
+## Tags
 
 Choose your PHP version by using the matching tag:
 
@@ -7,8 +8,19 @@ Choose your PHP version by using the matching tag:
 * `zeroseven07/docker-surf:php70` PHP 7.0
 * `zeroseven07/docker-surf:php71` PHP 7.1
 * `zeroseven07/docker-surf:php72` PHP 7.2
+* `zeroseven07/docker-surf:php72-composer1` PHP 7.2 + Composer 1
 * `zeroseven07/docker-surf:php73` PHP 7.3
+* `zeroseven07/docker-surf:php73-composer1` PHP 7.3 + Composer 1
 * `zeroseven07/docker-surf:php74` PHP 7.4
+* `zeroseven07/docker-surf:php74-composer1` PHP 7.4 + Composer 1
 * `zeroseven07/docker-surf:php80` PHP 8.0
 
 All available tags are listed on <https://hub.docker.com/r/zeroseven07/docker-surf/tags>
+
+## Changelog
+
+### 04.05.2021
+
+* Use composer 2 as default for php >= 7.2
+* Add composer 1 images for php 7.2, 7.3 & 7.4
+* Remove php72-webpack
--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 #docker build --pull . -f php70/Dockerfile -t zeroseven07/docker-surf:php70 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php70 /tmp/test.sh && \
 #docker build --pull . -f php71/Dockerfile -t zeroseven07/docker-surf:php71 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php71 /tmp/test.sh && \
 docker build --pull . -f php72/Dockerfile -t zeroseven07/docker-surf:php72 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php72 /tmp/test.sh && \
-docker build --pull . -f php72-webpack/Dockerfile -t zeroseven07/docker-surf:php72-webpack && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php72-webpack /tmp/test.sh && \
+docker build --pull . -f php72-composer1/Dockerfile -t zeroseven07/docker-surf:php72-composer1 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php72-composer1 /tmp/test.sh && \
 docker build --pull . -f php73/Dockerfile -t zeroseven07/docker-surf:php73 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php73 /tmp/test.sh && \
+docker build --pull . -f php73-composer1/Dockerfile -t zeroseven07/docker-surf:php73-composer1 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php73-composer1 /tmp/test.sh && \
 docker build --pull . -f php74/Dockerfile -t zeroseven07/docker-surf:php74 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php74 /tmp/test.sh && \
+docker build --pull . -f php74-composer1/Dockerfile -t zeroseven07/docker-surf:php74-composer1 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php74-composer1 /tmp/test.sh && \
 docker build --pull . -f php80/Dockerfile -t zeroseven07/docker-surf:php80 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven07/docker-surf:php80 /tmp/test.sh
